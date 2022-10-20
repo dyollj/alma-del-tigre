5caf4a54546160479474cb7349e8d6391a15ca21
Julian Lloyd <jlloyd@evernote.com>
2022-10-19 19:50:47 -0700

    Scaffold text layout inspection

--- screens/DemoScreen.tsx
+++ screens/DemoScreen.tsx
@@ -1,15 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Platform,
+  StyleSheet,
+  TextLayoutEventData,
+  NativeSyntheticEvent,
+  TextLayoutLine,
+} from "react-native";
 
-import { Text, View } from '../components/Themed';
+import { Text, View } from "../components/Themed";
 
 export default function DemoScreen() {
+  const [lines, setLines] = useState<TextLayoutLine[]>();
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>⻁</Text>
+      <Text
+        onTextLayout={(event: NativeSyntheticEvent<TextLayoutEventData>) => {
+          setLines(event.nativeEvent.lines);
+          console.log({ lines });
+        }}
+        style={styles.title}
+      >
+        This is a line of sample Evernote copy.
+      </Text>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
 }
@@ -17,11 +33,12 @@ export default function DemoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 60,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    color: "#00A82D",
   },
 });
